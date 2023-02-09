80d016b7aac767d7ce3aef0d4f55ea9dde238b3a
Robert Elizalde <devrced@outlook.com>
2023-02-09 01:08:47 -0600

    init commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+import set1.challenge1.HexToBase64
+fun main(args: Array<String>) {
+    var hexToBase64: HexToBase64 = HexToBase64()
+    //hexToBase64.hexToString("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6")
+    hexToBase64.stringToBinaryString("I'm killing your brain like a poisonous mushroom")
+}
